8a96c207a87250157fcf3c3b714de770abfa194f
wkkyu088 <dnjsrbwls08@gmail.com>
2023-05-30 16:54:31 +0900

    App에서 fetch, 3초마다 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { styled, Stack } from '@mui/material/'
 import MainPage from './MainPage';
 
 const App: React.FC = () => {
+  const [on, setOn] = useState(false);
+  const [pos, setPos] = useState(0);
+  const [age, setAge] = useState(0);
+  const [fontScale, setFontScale] = useState(1);
+  const [gender, setGender] = useState('');
+
+  // Fetch data from the API
+  const fetchData = async () => {
+      try {
+          const response = await axios.get('http://localhost:5000/detect_age');
+          console.log(response.data)
+
+          const value = response.data;
+          setPos(value.y-100>0 ? value.y-100 : 0);
+          setAge(value.age);
+          setFontScale(value.age===5 ? 1.2 : value.age<5 ? 1 : 1.5);
+          setGender(value.gender);
+      } catch (error) {
+          console.error('Failed to retrieve face coordinates:', error);
+      }
+  };
+
+  // Fetch data every second
+  useEffect(() => {
+      let interval: NodeJS.Timeout;
+
+      if (on) {
+          fetchData();
+          interval = setInterval(fetchData, 3000);
+      }
+
+      return () => {
+          clearInterval(interval);
+      };
+  }, [on]);
+
+  // Toggle the fetch data
+  const onToggle = () => {
+      setOn((prevOn) => !prevOn);
+      console.log(`fetch data : ${!on.toString()}`);
+  };
+
+  // Reset the fetch data
+  const onReset = () => {
+    setOn(false);
+    setPos(0);
+    setAge(0);
+    setFontScale(1); 
+    setGender('');
+    console.log(`Reset!`);
+};
+
   return (
-    <div>
-      <MainPage />
-    </div>
+    <Frame sx={{top: `${pos}px`}}>
+      <button onClick={onToggle}>Fetch Data: {on.toString()}</button>
+      <button onClick={onReset}>RESET</button>
+      <MainPage age={age} pos={pos} gender={gender} fontScale={fontScale} />
+      <button onClick={() => {
+        setGender(gender==="Male" ? "Female" : "Male");
+        setAge(age+1<8 ? age+1 : age); 
+        setFontScale(age+1===5 ? 1.2 : age+1<5 ? 1 : 1.5);
+        setPos(pos+50<251 ? pos+50 : pos);
+        }}>ALL TRANS TEST</button>
+    </Frame>
   );
 };
+
+const Frame = styled(Stack)(() => ({
+  position: 'relative',
+  transition: 'top 0.6s ease-in-out',
+  overflowX: 'hidden',
+  overflowY: 'hidden',
+}));
 
 export default App;
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,78 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Box, Button, Stack } from '@mui/material/'
-import { styled } from '@mui/material/styles';
+import React, { useState, useEffect } from 'react';
+import { styled, Box, Stack } from '@mui/material/'
 
-const MainPage: React.FC = () => {
-    const [on, setOn] = useState(false);
+const MainPage: React.FC<{age: number, pos: number, gender: string, fontScale: number}> = ({age, pos, gender, fontScale}) => {    
+    const ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
     const [currentTime, setCurrentTime] = useState(new Date());
-    const [pos, setPos] = useState(0);
 
-    const fetchData = async () => {
-    try {
-        const response = await axios.get('http://localhost:5000/face_coordinates');
-        console.log(response.data)
-
-        const value = response.data;
-        setPos(value.y-100>0 ? value.y-100 : 0);
-    } catch (error) {
-        console.error('Failed to retrieve face coordinates:', error);
-    }
-};
-
+    // Update current time every second
     useEffect(() => {
-        let interval: NodeJS.Timeout;
-
-        if (on) {
-            fetchData();
-            interval = setInterval(fetchData, 1000);
-        }
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [on]);
+        const interval = setInterval(() => {
+          setCurrentTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
-        <Body alignItems="center" justifyContent="space-between" sx={{top: `${pos}px`}}>
+        <Body alignItems="center" justifyContent="space-between">
             <Stack sx={{width: '100%'}}>
-                <Day>{currentTime.toLocaleDateString()}</Day>
-                <Time>{currentTime.toLocaleTimeString()}</Time>
+                <TransBox sx={{fontSize: `${20*fontScale}px`}}>{currentTime.toLocaleDateString()}</TransBox>
+                <TransBox sx={{fontSize: `${25*fontScale}px`}}>{currentTime.toLocaleTimeString()}</TransBox>
             </Stack>
-            <button onClick={() => setOn((prevOn) => !prevOn)}>Fetch Data: {on.toString()}</button>
-            <CustomBox alignItems="center" justifyContent="center">
-            </CustomBox>
-            <CustomText>Pos is {pos}px</CustomText>
+            <GenderBox alignItems="center" justifyContent="center" sx={{backgroundColor: gender==='Male' ? 'lightblue' : 'pink',}}>
+            </GenderBox>
+            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Pos is {pos}px</TransBox>
+            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Age is {ageList[age]}</TransBox>
+            <TransBox sx={{fontSize: `${20*fontScale}px`}}>FontScale is {fontScale}</TransBox>
+            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Gender is {gender}</TransBox>
         </Body>
     );
 };
 
 const Body = styled(Stack)(() => ({
-    position: 'relative',
     width: '100%',
     height: '400px',
     padding: '10px',
     color: 'white',
-    transition: 'top 0.6s ease-in-out',
 }));
 
-const Time = styled(Box)(() => ({
-    fontSize: '25px',
+const TransBox = styled(Box)(() => ({
+    transition: 'font-size 0.6s ease-in-out',
 }));
 
-const Day = styled(Box)(() => ({
-    fontSize: '20px',
-}));
-
-const CustomBox = styled(Stack)(() => ({
+const GenderBox = styled(Stack)(() => ({
     width: '80%',
-    backgroundColor: 'white',
     opacity: 0.5,
     height: '200px',
-}));
-
-const CustomText = styled(Box)(() => ({
-    fontSize: '20px',
+    transition: 'background-color 0.6s ease-in-out'
 }));
 
 export default MainPage;
