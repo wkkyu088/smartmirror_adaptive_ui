8974802ffd29c0da86f98dfda24f869521baa596
wkkyu088 <dnjsrbwls08@gmail.com>
2023-06-02 03:51:36 +0900

    내부 디자인 변경, 아이콘 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { styled, Stack } from '@mui/material/'
+import { styled, Stack, IconButton, Box } from '@mui/material/'
+import StartIcon from '@mui/icons-material/PlayArrowRounded';
+import PauseIcon from '@mui/icons-material/PauseRounded';
+import RefreshIcon from '@mui/icons-material/RefreshRounded';
+import DownIcon from '@mui/icons-material/ArrowDropDownCircleRounded';
+import TextUp from '@mui/icons-material/TextIncreaseRounded';
+import TextDown from '@mui/icons-material/TextDecreaseRounded';
+
 import MainPage from './MainPage';
 
 const App: React.FC = () => {
   const [on, setOn] = useState(false);
   const [pos, setPos] = useState(0);
   const [age, setAge] = useState(0);
+  const [age2, setAge2] = useState(0);
   const [fontScale, setFontScale] = useState(1);
   const [gender, setGender] = useState('');
+  const [screenHeight, setScreenHeight] = useState<number>(window.innerHeight);
+  const [fontSize, setFontSize] = useState(25);
 
   // Fetch data from the API
   const fetchData = async () => {
@@ -17,16 +27,18 @@ const App: React.FC = () => {
           console.log(response.data)
 
           const value = response.data;
-          setPos(value.y-100>0 ? value.y-100 : 0);
-          setAge(value.age);
-          setFontScale(value.age===5 ? 1.2 : value.age<5 ? 1 : 1.5);
+          setPos(value.y);
+          // setAge(value.age);
+          setAge2(value.age);
+          // setFontScale(value.age===5 ? 1.2 : value.age<5 ? 1 : 1.5);
+          setFontScale(value.age>55 ? 1.5 : value.age>38 ? 1.2 : 1);
           setGender(value.gender);
       } catch (error) {
           console.error('Failed to retrieve face coordinates:', error);
       }
   };
 
-  // Fetch data every second
+  // Fetch data every 3 seconds
   useEffect(() => {
       let interval: NodeJS.Timeout;
 
@@ -51,31 +63,80 @@ const App: React.FC = () => {
     setOn(false);
     setPos(0);
     setAge(0);
+    setAge2(0);
     setFontScale(1); 
     setGender('');
+    setFontSize(25);
     console.log(`Reset!`);
-};
+  };
+
+  // Increase the font size
+  const onClickUp = () => {
+    setFontSize(fontSize+5);
+    console.log({fontSize: fontSize+5});
+  };
+
+  // Decrease the font size
+  const onClickDown = () => {
+    setFontSize(fontSize-5);
+    console.log({fontSize: fontSize-5});
+  };
 
   return (
     <Frame sx={{top: `${pos}px`}}>
-      <button onClick={onToggle}>Fetch Data: {on.toString()}</button>
-      <button onClick={onReset}>RESET</button>
-      <MainPage age={age} pos={pos} gender={gender} fontScale={fontScale} />
-      <button onClick={() => {
+      <Stack direction="row" justifyContent="space-between" p={1}>
+        <Stack direction="row" alignItems="center">
+          <CustomIconButton onClick={onToggle}>
+            {
+              on ? <PauseIcon sx={{fontSize: '24px'}} /> : <StartIcon sx={{fontSize: '24px'}} />
+            }
+          </CustomIconButton>
+          <Box sx={{fontSize: '14px'}}>{on ? 'Now On' : 'Now Off'}</Box>
+        </Stack>
+        <Stack direction="row" spacing={0.5}>
+          <FontWidget onClick={onClickUp}>
+              <TextUp sx={{fontSize: '20px', marginRight: '2px'}} />
+              <Box>크게</Box>
+          </FontWidget>
+          <FontWidget onClick={onClickDown}>
+              <TextDown sx={{fontSize: '20px', marginRight: '2px'}} />
+              <Box>작게</Box>
+          </FontWidget>
+          <CustomIconButton onClick={onReset}><RefreshIcon sx={{fontSize: '24px'}} /></CustomIconButton>
+        </Stack>
+      </Stack>
+      <MainPage age={age2} pos={pos} gender={gender} fontScale={fontScale} fontSize={fontSize} />
+      <CustomIconButton onClick={() => {
         setGender(gender==="Male" ? "Female" : "Male");
-        setAge(age+1<8 ? age+1 : age); 
-        setFontScale(age+1===5 ? 1.2 : age+1<5 ? 1 : 1.5);
-        setPos(pos+50<251 ? pos+50 : pos);
-        }}>ALL TRANS TEST</button>
+        setAge2(age2+30<100 ? age2+30 : age2); 
+        setFontScale(age2+30>60 ? 1.5 : age2+30>40 ? 1.2 : 1);
+        setPos(pos+50<screenHeight-50 ? pos+50 : pos);
+        console.log({gender, age2, fontScale, pos});
+        }}><DownIcon /></CustomIconButton>
     </Frame>
   );
 };
 
 const Frame = styled(Stack)(() => ({
+  fontFamily: 'Pretendard',
   position: 'relative',
   transition: 'top 0.6s ease-in-out',
   overflowX: 'hidden',
   overflowY: 'hidden',
+}));
+
+const CustomIconButton = styled(IconButton)(() => ({
+  color: 'white',
+  fontSize: '14px',
+  padding: 0,
+}));
+
+const FontWidget = styled(IconButton)(() => ({
+  color: 'white',
+  padding: '3px 6px',
+  borderRadius: '10px',
+  border: '1px solid white',
+  fontSize: '14px'
 }));
 
 export default App;
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { styled, Box, Stack } from '@mui/material/'
+import { styled, Box, Stack, Grid, IconButton } from '@mui/material/'
+import CalendarIcon from '@mui/icons-material/TodayRounded';
+import WeatherIcon from '@mui/icons-material/WbSunnyRounded';
+import NewsIcon from '@mui/icons-material/NewspaperRounded';
+import ChecklistIcon from '@mui/icons-material/ChecklistRounded';
+import MusicIcon from '@mui/icons-material/MusicNoteRounded';
 
-const MainPage: React.FC<{age: number, pos: number, gender: string, fontScale: number}> = ({age, pos, gender, fontScale}) => {    
+const MainPage: React.FC<{age: number, pos: number, gender: string, fontScale: number, fontSize: number}> = ({age, pos, gender, fontScale, fontSize}) => {    
     const ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    
+    const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
     const [currentTime, setCurrentTime] = useState(new Date());
+
+    const iconList = [
+        { name: '달력', component: CalendarIcon, color: 'lightblue' },
+        { name: '날씨', component: WeatherIcon, color: 'lightyellow' },
+        { name: '뉴스', component: NewsIcon, color: 'lightgray' },
+        { name: '할 일', component: ChecklistIcon, color: 'pink' },
+        { name: '음악', component: MusicIcon, color: 'lightgreen' },
+    ]
 
     // Update current time every second
     useEffect(() => {
@@ -13,38 +28,79 @@ const MainPage: React.FC<{age: number, pos: number, gender: string, fontScale: n
         return () => clearInterval(interval);
     }, []);
 
+    // Update screen size when resized
+    useEffect(() => {
+        const handleResize = () => {
+          setScreenWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+        // console.log(`screenWidth: ${screenWidth}, screenHeight: ${screenHeight}`)
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <Body alignItems="center" justifyContent="space-between">
-            <Stack sx={{width: '100%'}}>
-                <TransBox sx={{fontSize: `${20*fontScale}px`}}>{currentTime.toLocaleDateString()}</TransBox>
-                <TransBox sx={{fontSize: `${25*fontScale}px`}}>{currentTime.toLocaleTimeString()}</TransBox>
-            </Stack>
-            <GenderBox alignItems="center" justifyContent="center" sx={{backgroundColor: gender==='Male' ? 'lightblue' : 'pink',}}>
-            </GenderBox>
-            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Pos is {pos}px</TransBox>
-            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Age is {ageList[age]}</TransBox>
-            <TransBox sx={{fontSize: `${20*fontScale}px`}}>FontScale is {fontScale}</TransBox>
-            <TransBox sx={{fontSize: `${20*fontScale}px`}}>Gender is {gender}</TransBox>
+        <Body alignItems="center">
+            <TransBox sx={{fontSize: `${(fontSize-4)*fontScale}px`}}>{currentTime.toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' })}</TransBox>
+            <TransBox sx={{fontSize: `${(fontSize)*fontScale}px`}}>{currentTime.toLocaleTimeString('ko-KR', { hour: 'numeric', minute: 'numeric', hour12: true })}</TransBox>
+            <GenderBox alignItems="center" justifyContent="center" sx={{backgroundColor: gender==='Male' ? 'lightblue' : 'pink',}} />
+            {/* <TransBox sx={{fontSize: `${(fontSize-10)*fontScale}px`}}>Pos: {pos}px, Age: {age}, Gender: {gender}</TransBox>
+            <TransBox sx={{fontSize: `${(fontSize-10)*fontScale}px`, paddingY: '10px'}}>FontScale: {fontScale}, FontSize: {fontSize}</TransBox> */}
+            <Grid container spacing={1} p={2} maxWidth="lg">
+                {
+                    iconList.map((icon, i) => {
+                        const IconComponent = icon.component;
+                        return <Grid item key={i} xs={fontScale===1 ? 12/5 : fontScale===1.2 ? 12/4 : 12/3}>
+                            <Stack alignItems="center" spacing={0.5}>
+                                <IconBox sx={{
+                                    backgroundColor: icon.color,
+                                    width: `${fontScale===1 ? screenWidth/5-15 : fontScale===1.2 ? screenWidth/4-15 : screenWidth/3-15}px`, 
+                                    height: `${fontScale===1 ? screenWidth/5-15 : fontScale===1.2 ? screenWidth/4-15 : screenWidth/3-15}px`,}} 
+                                >
+                                    <IconComponent sx={{fontSize: `${fontScale===1 ? screenWidth/5/2 : fontScale===1.2 ? screenWidth/4/2 : screenWidth/3/2}px`}} />
+                                </IconBox>
+                                <Box sx={{fontSize: `${(fontSize-12)*fontScale}px`}}>{icon.name}</Box>
+                            </Stack>
+                            </Grid>
+                    })
+                }
+            </Grid>
         </Body>
     );
 };
 
 const Body = styled(Stack)(() => ({
     width: '100%',
-    height: '400px',
-    padding: '10px',
+    // height: '400px',
     color: 'white',
+    marginTop: '10px',
 }));
 
 const TransBox = styled(Box)(() => ({
     transition: 'font-size 0.6s ease-in-out',
+    padding: '2px',
 }));
 
 const GenderBox = styled(Stack)(() => ({
     width: '80%',
     opacity: 0.5,
-    height: '200px',
+    margin: '10px',
+    height: '100px',
     transition: 'background-color 0.6s ease-in-out'
+}));
+
+const IconBox = styled(IconButton)(() => ({
+    color: 'black',
+    borderRadius: '20%',
+    transition: 'all 0.6s ease-in-out',
+    '&:hover': {
+        transform: 'scale(1.1)',
+        color: 'white',
+        // backgroundColor: 'white',
+    },
 }));
 
 export default MainPage;
